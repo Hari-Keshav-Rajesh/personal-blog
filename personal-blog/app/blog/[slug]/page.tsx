6583150c9bead8bd5e1c66f89558b91836c0b58c
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2024-01-21 19:54:27 +0530

    chore: add blog slug page design

--- personal-blog/app/blog/[slug]/page.tsx
+++ personal-blog/app/blog/[slug]/page.tsx
@@ -1,8 +1,14 @@
 import { NotionRenderer } from "react-notion";
 
+import Link from "next/link";
+
+import { Icons } from "@/config/icons";
+
 import "@/lib/styles.css";
 
 import { getPosts } from "../page";
+
+import { formatDate } from "@/lib/formatDate";
 
 async function getBlocks({ params: { slug } }: { params: { slug: any } }) {
     const posts = await getPosts();
@@ -37,9 +43,27 @@ export default async function BlogPost({
     const {post,blocks} = await getBlocks({ params: { slug: slug } });
 
     return(
-        <div>
+        <>
+            <Link href="/blog">
+                <div className="mt-5 flex items-center px-0 text-base font-medium text-muted-foreground transition-colors hover:text-foreground/80 xl:px-c8">
+                <Icons.arrowLeft className="h-5 w-5" />
+                <div>Back to Blog</div>
+                </div>
+            </Link>
+
+            <div className="mt-c20 px-0 pb-c3 md:mt-c10 xl:mt-c5 xl:px-c8">
+            <div className="mb-14 flex flex-col gap-4">
+            <div className="text-md font-medium">{formatDate(post.date)}</div>
+            <div className="text-4xl font-bold">{post.title}</div>
+            </div>
+
+            <div>
             <NotionRenderer blockMap={blocks} />
-        </div>
+            </div>
+      </div>
+
+        
+        </>
     )
 
 };
