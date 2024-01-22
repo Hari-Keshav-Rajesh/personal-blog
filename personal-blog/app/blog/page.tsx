e34435839bc81bc6a4b29fac2f71136dea398f3b
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2024-01-22 22:27:00 +0530

    feat: add carousel to home page

--- personal-blog/app/blog/page.tsx
+++ personal-blog/app/blog/page.tsx
@@ -24,7 +24,7 @@ import {
 export default async function Blog(){
 
     const posts: any = await getPosts();
-    
+
     return (
         <div className="mt-c20 px-0 pb-c3 md:mt-c10 xl:mt-c5 xl:px-c8">
           <div className="text-4xl font-bold">Latest Updates</div>
