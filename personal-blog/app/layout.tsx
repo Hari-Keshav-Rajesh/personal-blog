6bb19fa95ca623fa896858725edbfd410a9fc954
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2024-01-29 22:40:53 +0530

    feat: add site-footer

--- personal-blog/app/layout.tsx
+++ personal-blog/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SiteHeader from "@/components/ui/site-header";
+import SiteFooter from "@/components/ui/site-footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <SiteHeader />
         <div className="mx-10">{children}</div>
+        <SiteFooter />
       </body>
     </html>
   );
